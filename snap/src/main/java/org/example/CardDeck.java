d33782db047e355130da4bd83a7235dcbad655ba
unknown <sally-draycott@hotmail.co.uk>
2025-02-10 22:32:41 +0000

    added usesr functoinality

--- snap/src/main/java/org/example/CardDeck.java
+++ snap/src/main/java/org/example/CardDeck.java
@@ -1,9 +1,8 @@
 package org.example;
-
 import java.util.*;
 
-public class CardGame {
-    private ArrayList<Card> deckOfCards = new ArrayList<>(52);
+public class CardDeck {
+    private final ArrayList<Card> deckOfCards = new ArrayList<>(52);
 
     public void getDeck() {
         System.out.println("Here's your deck of cards:");
@@ -13,7 +12,7 @@ public class CardGame {
     }
 
     public Card dealCard() {
-        System.out.println("Here's the card from the top of the deck");
+//        System.out.println("Here's the card from the top of the deck");
         if(!deckOfCards.isEmpty()){
             System.out.println(deckOfCards.getFirst());
             return deckOfCards.removeFirst();
@@ -36,16 +35,8 @@ public class CardGame {
         System.out.println("Deck has been shuffled into suit order");
     }
 
-
-
-
-
-
-
-
-
-
-    public CardGame() {
+    static Scanner scanner = new Scanner(System.in);
+    public CardDeck() {
         System.out.println("Welcome to Snap!");
         deckOfCards.add(new Card("♥", "2", 2));
         deckOfCards.add(new Card("♥", "3", 3));
@@ -99,5 +90,34 @@ public class CardGame {
         deckOfCards.add(new Card("♠", "Q", 12));
         deckOfCards.add(new Card("♠", "K", 13));
         deckOfCards.add(new Card("♠", "A", 14));
+
+        System.out.println("Press enter to shuffle the deck");
+        String userInput = scanner.nextLine();
+        shuffleDeck();
+
+        System.out.println("You are ready to play Snap! \nPress enter to draw the first card");
+        String userInput2 = scanner.nextLine();
+        Card lastCard = dealCard();
+        System.out.println("Press enter to draw next card");
+        String userInput3 = scanner.nextLine();
+        Card currentCard = dealCard();
+
+        for(int card = 0; card < 53; card++) {
+            if (currentCard.getSymbol() == lastCard.getSymbol()) {
+                System.out.println("SNAP! YOU WIN");
+                break;
+            } else if (currentCard.getSymbol() != lastCard.getSymbol() && !deckOfCards.isEmpty()) {
+                lastCard = currentCard;
+                System.out.println("Press enter to draw next card");
+                String userInput4 = scanner.nextLine();
+                currentCard = dealCard();
+            } else if (deckOfCards.isEmpty()) {
+                System.out.println("Press enter to draw next card");
+                String userInput5 = scanner.nextLine();
+                System.out.println("YOU LOSE! The deck is empty. Goodbye...");
+                break;
+            } else System.out.println("Sorry we have encountered an unforeseen error, please restart the game");;
+        }
+
     }
 }
